31a2430401b10d093353691bd719bdf422ca179d
DevRasalas <agusportill21@gmail.com>
2023-10-17 16:15:36 -0300

    Primer commit

---
+++ src/app/producto-lista/producto-lista.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
+
+@Component({
+  selector: 'app-producto-lista',
+  templateUrl: './producto-lista.component.html'
+})
+export class ProductoListaComponent {
+  productos: Producto[];
+
+  constructor(private productoServicio: ProductoService){};
+  ngOnInit(){
+    this.obtenerProductos();
+  }
+  private obtenerProductos(){
+    this.productoServicio.obtenerListaProductos().subscribe(
+      (datos => {
+        this.productos = datos;
+      })
+    );
+  }
+  
+}
